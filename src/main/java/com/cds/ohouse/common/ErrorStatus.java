d84479b72ba2fa0af6671be058c3503b5bb38a21
dragontaek-lee <dragontaek98@naver.com>
2023-05-18 01:31:59 +0900

    feat: init global exception handler #3

--- src/main/java/com/cds/ohouse/common/ErrorStatus.java
+++ src/main/java/com/cds/ohouse/common/ErrorStatus.java
@@ -13,6 +13,7 @@ public enum ErrorStatus {
      * 400 BAD REQUEST
      */
     REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
+    VALIDATION_REQUEST_MISSING_EXCEPTION(HttpStatus.BAD_REQUEST, "요청값이 입력되지 않았습니다."),
 
     /**
      * 404 NOT FOUND
