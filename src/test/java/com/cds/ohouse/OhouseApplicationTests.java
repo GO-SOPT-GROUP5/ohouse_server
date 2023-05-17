39bb50717d6161f94f0feb4511345c20ca58a18d
dragontaek-lee <dragontaek98@naver.com>
2023-05-17 15:40:58 +0900

    feat: init project

---
+++ src/test/java/com/cds/ohouse/OhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cds.ohouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
