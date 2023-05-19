275c28e9ac26ca7ab3132fef73b354c97f40d46d
dragontaek-lee <dragontaek98@naver.com>
2023-05-20 03:10:48 +0900

    feat: category service #9

---
+++ src/main/java/com/cds/ohouse/service/CategoryService.java
@@ -0,0 +1,6 @@
+package com.cds.ohouse.service;
+
+import com.cds.ohouse.dto.request.CategoryUpdateRequestDTO;
+public interface CategoryService {
+    void updateCategory(CategoryUpdateRequestDTO categoryUpdateRequestDTO);
+}
