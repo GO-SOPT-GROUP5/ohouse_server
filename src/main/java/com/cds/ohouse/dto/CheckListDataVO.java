da561c88b2f4272e32131038a1429629e62b5105
dragontaek-lee <dragontaek98@naver.com>
2023-05-24 00:09:47 +0900

    fix: unused DTO #21

--- src/main/java/com/cds/ohouse/dto/CheckListDataVO.java
+++
@@ -1,8 +0,0 @@
-package com.cds.ohouse.dto;
-
-import com.cds.ohouse.domain.SubCategoryStatus;
-
-public class CheckListDataVO {
-  private SubCategoryStatus subCategoryStatus;
-  private int status;
-}
